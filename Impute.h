21fdcdbc2ab1f0fee9e29358cd1d6ae73840b8da
yjingj <yjingj@gmail.com>
2014-12-09 14:22:26 -0500

    mv impute.h to Impute.h

---
+++ Impute.h
@@ -0,0 +1,17 @@
+#include "VcfFileReader.h"
+#include "StringBasics.h"
+#include "StringHash.h"
+#include "MemoryAllocators.h"
+#include "CalcGL.h"
+
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+
+#include <iostream>     // std::cout, std::endl
+#include <iomanip>
+
+using namespace std
+
+
+
